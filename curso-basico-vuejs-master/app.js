45e68be0acca3fba22ec3b7a9466e9ebe5892141
AndresAzuara <andres.azuara.portillo@outlook.com>
2020-04-20 23:38:01 -0500

    Initial version

---
+++ curso-basico-vuejs-master/app.js
@@ -0,0 +1,81 @@
+Vue.component('CoinDetail', {
+    props:['coin'],
+    data(){
+        return {
+            showPrices: false,
+            value: 0
+        }
+    },
+    methods: {
+        toggleShowPrices(){
+            this.showPrices = !this.showPrices;
+        }
+    },
+    computed: {
+        convertedValue(){
+            if(!this.value){
+                return 0;
+            }
+            return this.value / this.coin.price;
+        },
+        entireName(){
+            return `${this.coin.name} - ${this.coin.symbol}`
+        }
+    },
+    template: `
+    <div>
+    <img
+        v-on:mouseover="toggleShowPrices"
+        v-on:mouseout="toggleShowPrices"
+        v-bind:src="coin.img"
+        v-bind:alt="coin.name"
+    />
+    <h1 v-bind:class="coin.changePercent > 0? 'green' : 'red'">
+    {{ coin.name }}
+    <span v-if="coin.changePercent > 0">c:</span>
+    <span v-else-if="coin.changePercent < 0">:c</span>
+    <span v-else>:|</span>
+
+    <span v-on:click="toggleShowPrices"> {{ this.showPrices? "Ocultar precios" : "Ver Precios" }}</span>
+  </h1>
+  <input type="number" v-model="value"/>
+  <span>{{convertedValue}}</span>
+  <ul v-show="this.showPrices">
+    <li class="uppercase" v-bind:class="{orange: p.value == coin.price, red: p.value < coin.price, green: p.value > coin.price}" v-for="(p, i) in coin.pricesWithDays" v-bind:key="p.day">{{p.day}} - {{p.value}}</li>
+  </ul>
+    </div>
+    `
+})
+
+new Vue({
+    el: "#app",
+    data(){
+        return {
+            btc: {
+                name: "Bitcoin",
+                symbol: "BTN",
+                img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+                changePercent: 10,
+                pricesWithDays: [
+                    { day: "Lunes", value: 8400 },
+                    { day: "Martes", value: 7900 },
+                    { day: "Miercoles", value: 8200 },
+                    { day: "Jueves", value: 9000 },
+                    { day: "Viernes", value: 9400 },
+                    { day: "Sabado", value: 10000 },
+                    { day: "Domingo", value: 10200 }
+                ],
+                price: 8400
+            },
+            color: "F4F4F4",
+        }
+    }
+    //methods: {
+    //    toggleShowPrices(){
+    //        this.showPrices = !this.showPrices;
+    //        this.color = this.color.split('')
+    //            .reverse()
+    //            .join('')
+    //    }
+    //}
+})
